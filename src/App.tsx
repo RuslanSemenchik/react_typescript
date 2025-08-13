389b780a7dfbe4f0902eb8fd3ad6e00c3825f273
RuslanSemenchik <semenchik.r@gmail.com>
2025-08-13 18:38:01 +0200

    add-homework06 (#1)
    * add-homework06
    
    * fix type car

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Homework_06 from './homeworks/Homework_06/Homework_06'
 import Lesson_06 from './lessons/Lesson_06/Lesson_06'
 
 function App() {
   return <div className="app">
-    <Lesson_06/>
+    {/* <Lesson_06/> */}
+    <Homework_06/>
 
   </div>
   
