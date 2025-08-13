606e0a88194f2ecf0becdb7fd67e6b47a549c7ef
RuslanSemenchik <semenchik.r@gmail.com>
2025-08-13 11:18:05 +0200

    add-lesson06

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 
 import './App.css'
+import Lesson_06 from './lessons/Lesson_06/Lesson_06'
 
 function App() {
-  return <div className="app">MY TYPESCRIPT</div>
+  return <div className="app">
+    <Lesson_06/>
+
+  </div>
   
 }
+
 
 export default App
