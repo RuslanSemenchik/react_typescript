c82aa045838194ddc5bf62c916bdc3865d5afe05
CureForCode <artursomov@yahoo.de>
2025-08-26 14:59:18 +0200

    Пункты 1 и 2 проекта выполнены. Подготовлен макет/шаблон для дальнейшей командной доработки.

--- src/lessons/Lesson_10/Lesson_10.tsx
+++ src/lessons/Lesson_10/Lesson_10.tsx
@@ -1,34 +1,49 @@
-// Разместите на странице Input с label="Country", placeholder="Enter Counry for searching uneversities",
-//  в который пользователь может ввести название страны для посика университетов
-// Разместите на странице Button "Get Universities", по клику на которую, отправляется GET запрос 
-// на http://universities.hipolabs.com/search?country=COUNTRY_NAME
-// Используйте для запросов axios
-// Если в ответе на запрос пришли нормальные данные(запрос выполнен успешно), то разместите данные в стейте,
-//  а затем отобразите на стрнице в виде каточек. Сохраняйте не более 15 обьектов в массиве в стейт
-// При повторном нажатии на кнопку "Get Universities",
-//  выполните новый GET запрос на http://universities.hipolabs.com/search?country=COUNTRY_NAME и 
-// получите новые данные, 
-// и обновите стейт
-// При получении ошибки, положите в отдельный стейт данные об ошибке с соббщением "Some Network Error"
-// Стили на ваше усмотрение, контент тоже на ваше усмотрение
-// Все нужно делать в компоненте Lesson_10
- import { PageWrapper } from "./styles";
+import { useState } from "react";
+import { PageWrapper } from "./styles";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
- function Lesson_10 (){
+// Функция, которая собирает корректный URL и сохраняет его в LAST_URL.
+import { buildUniversitiesUrl } from "./data";
 
+function Lesson_10() {
+  // Локальное состояние для ввода страны пользователем.
+  const [country, setCountry] = useState<string>("");
 
+  // Обработчик клика по кнопке "Get Universities".
+  const handleGetClick = () => {
+    const query = country.trim();
+    // Если поле пустое, то выходим и запрос не формируем.
+    if (!query) return;
 
+    const url = buildUniversitiesUrl(query);
+    // Пока просто выводится адрес в консоль. Это проверка, что всё собралось верно. Далее этот момент можно убрать или изменить.
+    console.log("Lesson_10 formed url:", url);
+    // Здесь нужно будет поработать над axios и дальнейшей обработкой данных и/или ошибок.
+  };
 
-
-
-    return <PageWrapper>
-
-
-
-
+  return (
+    <PageWrapper>
+      <div>
+        {/* Поле ввода: контролируемое значение (value) + onChange обновляет state */}
+        <Input
+          id="countryInput"
+          name="country"
+          type="text"
+          label="Country"
+          placeholder="Enter Country for searching universities"
+          value={country} // текущее значение из state
+          onChange={(e) => setCountry(e.target.value)} // пишем в state при каждом вводе
+        />
+        {/* Кнопка запуска действия. По клику вызываем handleGetClick */}
+        <Button
+          type="button"
+          buttonName="Get Universities"
+          onClick={handleGetClick}
+        />
+      </div>
     </PageWrapper>
+  );
+}
 
-
- }
- export default Lesson_10;
-
+export default Lesson_10;
